520e6eca44b80b35da07041723c315ac7bddd86b
mahmud farsi <mhfarsi002022@gmail.com>
2024-04-15 18:26:52 +0330

    start project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="capitalize">
+    <Input placeholder="search" type="text" id="search"/>
+  </div>
+</template>
+
+<script setup>
+  import Input from '@/components/base/Input.vue'
+</script>
---
+++ src/components/base/Input.vue
@@ -0,0 +1,27 @@
+<template>
+    <input :type="type" :placeholder="placeholder" :id="id"/>
+</template>
+
+<script setup>
+    import { defineProps,computed } from 'vue';
+    import { string,bool } from 'vue-types';
+
+    defineProps({
+        type:string(),
+        placeholder:string(),
+        id:string(),
+        isBorderB:bool().def(false),
+        isBorder:bool().def(false),
+        isPlaceholderMd:bool().def(false),
+        isPlaceholderLg:bool().def(false),
+
+    })
+
+
+    const elementClass = computed(() =>({
+        'is-border-bottom': props.isBorderB,
+        'is-border': props.isBorder,
+        'placeholder-md': props.isPlaceholderMd,
+        'placeholder-lg':  props.isPlaceholderLg
+    }))
+</script>
